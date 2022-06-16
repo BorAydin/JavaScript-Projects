915463325631b60caaf298654f82a44843f2c768
Bora Aydın <boraaydins@gmail.com>
2022-06-16 04:48:43 +0300

    Add files via upload

---
+++ Artan Sayaç/script.js
@@ -0,0 +1,26 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+  
+  counter.innerText = '0'
+
+  const updateCounter = () => {
+    
+    const target = +counter.getAttribute('data-target') // String dönüyordu + ile Number'a çevirdi.
+    const c = +counter.innerText
+
+    const increment = target / 600
+
+    if (c < target){
+
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+
+    } else{
+      counter.innerText = target
+    }
+  }
+
+  updateCounter()
+
+})
