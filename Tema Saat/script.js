8286eb783713cb6c652e0434ad17b0927157f3d7
Bora Aydın <boraaydins@gmail.com>
2022-08-12 00:20:24 +0300

    Add files via upload

---
+++ Tema Saat/script.js
@@ -0,0 +1,51 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Pazar","Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
+const months = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
+
+toggle.addEventListener('click', (e) => {
+  
+  const html = document.querySelector('html')
+
+  if(html.classList.contains('dark')){
+    html.classList.remove('dark')
+    e.target.innerHTML = "Koyu mod"
+  } else {
+    html.classList.add('dark')
+    e.target.innerHTML = "Açık mod"
+  }
+
+})
+
+function setTime() {
+  
+  const time = new Date();
+  const month = time.getMonth()
+  const day = time.getDay()
+  const date = time.getDate()
+  const hours = time.getHours()
+  const hoursForClock = hours % 24
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 23, 0, 360)}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hoursForClock< 10 ? `0${hoursForClock}` : hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}`
+  dateEl.innerHTML= `${days[day]}, ${months[month]} <span class = "circle">${date}`
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
